d8a4576c2443a34a5d5d904b2803d8ab91ff0d2c
Azecko <samikarin24@gmail.com>
2024-11-20 15:16:40 +0100

    [feature] With apprenticeship contract form
    Probably need to put that page under another route than the main route, but yeah

---
+++ app/components/EPCLVaudSignature.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+export default function EPCLVaudSignature() {
+    return (
+        <div className="etat-de-vaud-div flex flex-row items-center gap-4">
+            <Image className="etat-de-vaud-logo" alt="Logo de l'état de vaud" src="/etat_de_vaud_logo.webp" width="45" height="200"/>
+            <div className="etat-de-vaud-separator bg-black w-[0.5px] h-40"></div>
+            <p className="etat-de-vaud-text">
+                <strong>Ecole professionnelle commerciale de Lausanne</strong><br />
+                Chemin de la Prairie 11<br />
+                Case postale 272<br />
+                1000 Lausanne 16<br />
+                Rue du Midi 13<br />
+                1003 Lausanne<br />
+            </p>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,118 @@
-import Image from "next/image";
+import EPCLVaudSignature from "./components/EPCLVaudSignature";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <div className="px-24 flex items-end justify-between">
+          <EPCLVaudSignature />
+        <span className="text-white bg-gray-400 font-semibold text-7xl pt-48 px-2">S16</span>
+      </div>
+      <div className="flex flex-col items-center mt-12 mb-4">
+        <h3 id="main-title" className="font-bold text-xl">Justification d&apos;absence à transmettre via le formulaire en ligne</h3>
+        <h4 id="main-description">pour élèves <span className="font-semibold underline">AVEC</span> contrat d&apos;apprentissage</h4>
+      </div>
+      <div className="flex flex-col w-fit mx-auto">
+        <h3 id="EPCL-reserves-right" 
+        className="
+          text-center
+          text-lg font-bold
+          px-48
+          border
+          border-black
+          italic"
+        >
+          L&apos;EPCL se réserve le droit de demander un certificat médical.
+        </h3>
+        {/* "Elève" section */}
+        <div className="mt-16 border border-black">
+          <div className="px-2 bg-gray-300">
+            <span className="section-title">Elève</span>
+          </div>
+          <div className="mt-4 px-2">
+            <label className="labels" htmlFor="student-name">Nom : </label>
+            <input placeholder={".".repeat(500)} type="text" id="student-name" />
+            <label className="labels" htmlFor="student-firstname">Prénom : </label>
+            <input placeholder={".".repeat(500)} type="text" id="student-firstname" />
+            <label className="labels" htmlFor="student-class">Classe : </label>
+            <input className="w-24" placeholder={".".repeat(500)} type="text" id="student-class" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        {/* "Absence" section */}
+        <div className="mt-8 border border-black">
+          <div className="px-2 bg-gray-300">
+            <span className="section-title">Absence</span>
+          </div>
+          <div className="flex flex-col gap-2 mt-4 px-2">
+            <div>
+              <label className="labels" htmlFor="absence-from">Du (début de l&apos;absence) : date : </label>
+              <input placeholder={".".repeat(500)} type="text" id="absence-from" />
+              <label className="labels" htmlFor="absence-from-hour">, heure : </label>
+              <input className="w-12" placeholder={".".repeat(500)} type="text" id="absence-from-hour" />
+            </div>
+            <div>
+              <label className="labels" htmlFor="absence-to">Au (fin de l&apos;absence) : date :</label>
+              <input placeholder={".".repeat(500)} type="text" id="absence-to" />
+              <label className="labels" htmlFor="absence-to-hour">, heure : </label>
+              <input className="w-12" placeholder={".".repeat(500)} type="text" id="absence-to-hour" />
+            </div>
+          </div>
+        </div>
+        {/* "Tests manqués" section */}
+        <div className="mt-8 border border-black">
+          <div className="px-2 bg-gray-300">
+            <span className="section-title">Test(s) manqué(s) dans la/les branche(s) suivante(s)</span>
+          </div>
+          <div className="mt-4 px-2">
+            <textarea className="w-full resize-none" rows={4} name="missed-subjects" id="missed-subjects"></textarea>
+          </div>
+        </div>
+        {/* "Motif de l'absence" section */}
+        <div className="mt-8 border border-black">
+          <div className="px-2 bg-gray-300">
+            <span className="section-title">Motif de l&apos;absence <span className="italic text-xs">(obligatoire)</span></span>
+          </div>
+          <div className="mt-4 px-2">
+            <textarea className="w-full resize-none" rows={4} name="absence-reason" id="absence-reason"></textarea>
+          </div>
+        </div>
+        {/* "Validations" section */}
+        <div className="mt-4">
+          <h4 className="font-semibold underline validations-title">Validations</h4>
+          <div className="mt-4 border border-black">
+            <div className="px-2 bg-gray-300">
+              <span className="section-title">Responsable de formation</span>
+            </div>
+            <div className="mt-4 px-2">
+              <label className="labels" htmlFor="responsible-name">Nom : </label>
+              <input placeholder={".".repeat(500)} type="text" id="responsible-name" />
+              <label className="labels" htmlFor="responsible-firstname">Prénom : </label>
+              <input placeholder={".".repeat(500)} type="text" id="responsible-firstname" />
+              <label className="labels" htmlFor="responsible-phone">No tél : </label>
+              <input placeholder={".".repeat(500)} type="text" id="responsible-phone" />
+            </div>
+          </div>
+        </div>
+        {/* "Signatures" section */}
+        <div className="flex flex-col gap-6 mt-24 px-2">
+          <div className="flex justify-between">
+            <span className="certifications italic">Certifié conforme par l&apos;employeur</span>
+            <div className="flex flex-col w-3/5">
+              <input className="w-full" placeholder={".".repeat(500)} type="text" />
+              <span className="text-sm italic">Lieu, date et signature du responsable de formation (obligatoire)</span>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <span className="certifications italic">Certifié conforme par l&apos;élève :</span>
+              <span className="certifications italic">Ou son représentant légal si mineur</span>
+            </div>
+            <div className="flex flex-col w-3/5">
+              <input className="w-full" placeholder={".".repeat(500)} type="text" />
+              <span className="text-sm italic">Signature de l’élève ou du représentant légal</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
